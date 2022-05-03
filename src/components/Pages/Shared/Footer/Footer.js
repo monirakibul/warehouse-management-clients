da959d1a51680bd539872fc5340d1893880e7385
monirakibul <monirakibul@gmail.com>
2022-05-04 01:59:35 +0600

    slider added

--- src/components/Pages/Shared/Footer/Footer.js
+++ src/components/Pages/Shared/Footer/Footer.js
@@ -2,10 +2,13 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 const Footer = () => {
+    const today = new Date();
+    const year = today.getFullYear();
+
     return (
         <div className='border-t-2 border-gray-100 bg-white rounded-lg shadow'>
             <footer className="max-w-6xl mx-auto px-4 p-4  md:flex md:items-center md:justify-between">
-                <span className="text-sm text-gray-500 sm:text-center dark:text-gray-400">© 2022 <Link to="/" className="hover:underline">Quick Store</Link>. All Rights Reserved.
+                <span className="text-sm text-gray-500 sm:text-center dark:text-gray-400">© {year} <Link to="/" className="hover:underline">Quick Store</Link>. All Rights Reserved.
                 </span>
                 <ul className="flex flex-wrap justify-center items-center mt-3 text-sm text-gray-500  sm:mt-0">
                     <li>
